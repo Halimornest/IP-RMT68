107d65fea8095ed3675cf2e44c80a104967c4bca
halimornest <halimornest14@gmail.com>
2026-01-24 09:49:57 +0700

    Test

--- server/src/server.js
+++ server/src/server.js
@@ -1,17 +1,7 @@
 const app = require("./app");
-const { sequelize } = require("./config/database");
 
 const PORT = process.env.PORT || 3000;
 
-(async () => {
-  try {
-    await sequelize.authenticate();
-    console.log("Database connected");
-
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  } catch (err) {
-    console.error(err);
-  }
-})();
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
