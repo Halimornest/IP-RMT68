a2a565c9c8f601479cae117f24bd9a561ffb2c2d
halimornest <halimornest14@gmail.com>
2026-01-24 20:54:52 +0700

    Test

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,16 +1,26 @@
-require('dotenv').config() 
-
+require('dotenv').config()
 const { Sequelize } = require('sequelize')
 
-const sequelize = new Sequelize(
-  process.env.DB_NAME,
-  process.env.DB_USER,
-  String(process.env.DB_PASS), 
-  {
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-    logging: false,
-  }
-)
+const sequelize = process.env.DATABASE_URL
+  ? new Sequelize(process.env.DATABASE_URL, {
+      dialect: 'postgres',
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+      logging: false,
+    })
+  : new Sequelize(
+      process.env.DB_NAME,
+      process.env.DB_USER,
+      process.env.DB_PASS,
+      {
+        host: process.env.DB_HOST,
+        dialect: 'postgres',
+        logging: false,
+      }
+    )
 
 module.exports = { sequelize }
